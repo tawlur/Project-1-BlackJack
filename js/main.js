6bc0618ad2901015873f179600babd4511cb8034
Macbook Pro <macbookpro@Tawlur-MBP.hsd1.co.comcast.net>
2020-05-09 22:31:19 -0600

    Created base of shuffle and card dealing logic

--- js/main.js
+++ js/main.js
@@ -25,15 +25,10 @@
 
 /*---Functions---*/
 
+
 // function init() {
 
 
-//     deck = ["dA","dQ","dK","dJ","d10","d09","d08",
-//     "d07","d06","d05","d04","d03","d02","hA","hQ","hK",
-//     "hJ","h10","h09","h08","h07","h06","h05","h04","h03",
-//     "h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06",
-//     "c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09",
-//     "s08","s07","s06","s05","s04","s03","s02"];
 
 //     render();
 // }
@@ -47,9 +42,8 @@ let deck = ["dA","dQ","dK","dJ","d10","d09","d08",
 "s08","s07","s06","s05","s04","s03","s02"];
 
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-    console.log(getRandomInt(52));
-    //create random number between 0 and 51 to pull from deck
-  }
-  console.log(getRandomInt(52));    
+var hand = deck.splice(Math.floor(Math.random()*deck.length),1);
+    console.log(hand)
+var hand = deck.splice(Math.floor(Math.random()*deck.length),1);
+console.log(hand);
+console.log(deck);
