1c0c8d7b2db9dc382adf0890b19a19d5bb69f4e8
Macbook Pro <macbookpro@Tawlur-MBP.hsd1.co.comcast.net>
2020-05-10 16:17:43 -0600

    Random Deal functionality functional

--- js/main.js
+++ js/main.js
@@ -4,7 +4,13 @@
 
 
 /*---Variables---*/
-
+const hand = [];
+let deck = ["dA","dQ","dK","dJ","d10","d09","d08",
+"d07","d06","d05","d04","d03","d02","hA","hQ","hK",
+"hJ","h10","h09","h08","h07","h06","h05","h04","h03",
+"h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06",
+"c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09",
+"s08","s07","s06","s05","s04","s03","s02"];
 
 
    //vaires state from start to shuffle
@@ -24,6 +30,7 @@
 
 
 /*---Functions---*/
+const getSelectedCard = () => deck.splice(Math.floor(Math.random()*deck.length),1)
 
 
 // function init() {
@@ -34,16 +41,19 @@
 // }
 
 
-let deck = ["dA","dQ","dK","dJ","d10","d09","d08",
-"d07","d06","d05","d04","d03","d02","hA","hQ","hK",
-"hJ","h10","h09","h08","h07","h06","h05","h04","h03",
-"h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06",
-"c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09",
-"s08","s07","s06","s05","s04","s03","s02"];
 
 
-var hand = deck.splice(Math.floor(Math.random()*deck.length),1);
-    console.log(hand)
-var hand = deck.splice(Math.floor(Math.random()*deck.length),1);
+let selectedCard = getSelectedCard();
+  hand.push(selectedCard[0])
 console.log(hand);
+
+selectedCard = getSelectedCard();
+hand.push(selectedCard[0])
+console.log(hand);
+
+//draw a new random card
+hand.push(selectedCard[0])
+console.log(hand);
+
 console.log(deck);
+
