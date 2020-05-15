bb901dec18a2df97c77d1e3881ae294bdacdd41f
Macbook Pro <macbookpro@Tawlur-MBP.hsd1.co.comcast.net>
2020-05-15 10:10:38 -0600

    Seperate js Deck

--- js/main.js
+++ js/main.js
@@ -1,215 +1,215 @@
 /*---Variables---*/
 
-let deck = [
-  {
-      name: "dA",
-      value: 11,
-  },
-  {
-      name: "dQ",
-      value: 10,
-  },
-  {
-      name: "dK",
-      value: 10,
-  },
-  {
-      name: "dJ",
-      value: 10,
-  },
-  {
-      name: "d10",
-      value: 10,
-  },
-  {
-      name: "d09",
-      value: 9,
-  },
-  {
-      name: "d08",
-      value: 8,
-  },
-  {
-      name: "d07",
-      value: 7,
-  },
-  {
-      name: "d06",
-      value: 6,
-  },
-  {
-      name: "d05",
-      value: 5,
-  },
-  {
-      name: "d04",
-      value: 4,
-  },
-  {
-      name: "d03",
-      value: 3,
-  },
-  {
-      name: "d02",
-      value: 2,
-  },
-  {
-      name: "hA",
-      value: 11,
-  },
-  {
-      name: "hQ",
-      value: 10,
-  },
-  {
-      name: "hK",
-      value: 10,
-  },
-  {
-      name: "hJ",
-      value: 10,
-  },
-  {
-      name: "h10",
-      value: 10,
-  },
-  {
-      name: "h09",
-      value: 9,
-  },
-  {
-      name: "h08",
-      value: 8,
-  },
-  {
-      name: "h07",
-      value: 7,
-  },
-  {
-      name: "h06",
-      value: 6,
-  },
-  {
-      name: "h05",
-      value: 5,
-  },
-  {
-      name: "h04",
-      value: 4,
-  },
-  {
-      name: "h03",
-      value: 3,
-  },
-  {
-      name: "h02",
-      value: 2,
-  },
-  {
-      name: "cA",
-      value: 11,
-  },
-  {
-      name: "cQ",
-      value: 10,
-  },
-  {
-      name: "cK",
-      value: 10,
-  },
-  {
-      name: "cJ",
-      value: 10,
-  },
-  {
-      name: "c10",
-      value: 10,
-  },
-  {
-      name: "c09",
-      value: 9,
-  },
-  {
-      name: "c08",
-      value: 8,
-  },
-  {
-      name: "c07",
-      value: 7,
-  },
-  {
-      name: "c06",
-      value: 6,
-  },
-  {
-      name: "c05",
-      value: 5,
-  },
-  {
-      name: "c04",
-      value: 4,
-  },
-  {
-      name: "c03",
-      value: 3,
-  },
-  {
-      name: "c02",
-      value: 2,
-  },
-  {
-      name: "sA",
-      value: 11,
-  },
-  {
-      name: "sQ",
-      value: 10,
-  },
-  {
-      name: "sK",
-      value: 10,
-  },
-  {
-      name: "sJ",
-      value: 10,
-  },
-  {
-      name: "s10",
-      value: 10,
-  },
-  {
-      name: "s09",
-      value: 9,
-  },
-  {
-      name: "s08",
-      value: 8,
-  },
-  {
-      name: "s07",
-      value: 7,
-  },
-  {
-      name: "s06",
-      value: 6,
-  },
-  {
-      name: "s05",
-      value: 5,
-  },
-  {
-      name: "s04",
-      value: 4,
-  },
-  {
-      name: "s03",
-      value: 3,
-  },
-  {
-      name: "s02",
-      value: 2,
-  }
-];
+// let deck = [
+//   {
+//       name: "dA",
+//       value: 11,
+//   },
+//   {
+//       name: "dQ",
+//       value: 10,
+//   },
+//   {
+//       name: "dK",
+//       value: 10,
+//   },
+//   {
+//       name: "dJ",
+//       value: 10,
+//   },
+//   {
+//       name: "d10",
+//       value: 10,
+//   },
+//   {
+//       name: "d09",
+//       value: 9,
+//   },
+//   {
+//       name: "d08",
+//       value: 8,
+//   },
+//   {
+//       name: "d07",
+//       value: 7,
+//   },
+//   {
+//       name: "d06",
+//       value: 6,
+//   },
+//   {
+//       name: "d05",
+//       value: 5,
+//   },
+//   {
+//       name: "d04",
+//       value: 4,
+//   },
+//   {
+//       name: "d03",
+//       value: 3,
+//   },
+//   {
+//       name: "d02",
+//       value: 2,
+//   },
+//   {
+//       name: "hA",
+//       value: 11,
+//   },
+//   {
+//       name: "hQ",
+//       value: 10,
+//   },
+//   {
+//       name: "hK",
+//       value: 10,
+//   },
+//   {
+//       name: "hJ",
+//       value: 10,
+//   },
+//   {
+//       name: "h10",
+//       value: 10,
+//   },
+//   {
+//       name: "h09",
+//       value: 9,
+//   },
+//   {
+//       name: "h08",
+//       value: 8,
+//   },
+//   {
+//       name: "h07",
+//       value: 7,
+//   },
+//   {
+//       name: "h06",
+//       value: 6,
+//   },
+//   {
+//       name: "h05",
+//       value: 5,
+//   },
+//   {
+//       name: "h04",
+//       value: 4,
+//   },
+//   {
+//       name: "h03",
+//       value: 3,
+//   },
+//   {
+//       name: "h02",
+//       value: 2,
+//   },
+//   {
+//       name: "cA",
+//       value: 11,
+//   },
+//   {
+//       name: "cQ",
+//       value: 10,
+//   },
+//   {
+//       name: "cK",
+//       value: 10,
+//   },
+//   {
+//       name: "cJ",
+//       value: 10,
+//   },
+//   {
+//       name: "c10",
+//       value: 10,
+//   },
+//   {
+//       name: "c09",
+//       value: 9,
+//   },
+//   {
+//       name: "c08",
+//       value: 8,
+//   },
+//   {
+//       name: "c07",
+//       value: 7,
+//   },
+//   {
+//       name: "c06",
+//       value: 6,
+//   },
+//   {
+//       name: "c05",
+//       value: 5,
+//   },
+//   {
+//       name: "c04",
+//       value: 4,
+//   },
+//   {
+//       name: "c03",
+//       value: 3,
+//   },
+//   {
+//       name: "c02",
+//       value: 2,
+//   },
+//   {
+//       name: "sA",
+//       value: 11,
+//   },
+//   {
+//       name: "sQ",
+//       value: 10,
+//   },
+//   {
+//       name: "sK",
+//       value: 10,
+//   },
+//   {
+//       name: "sJ",
+//       value: 10,
+//   },
+//   {
+//       name: "s10",
+//       value: 10,
+//   },
+//   {
+//       name: "s09",
+//       value: 9,
+//   },
+//   {
+//       name: "s08",
+//       value: 8,
+//   },
+//   {
+//       name: "s07",
+//       value: 7,
+//   },
+//   {
+//       name: "s06",
+//       value: 6,
+//   },
+//   {
+//       name: "s05",
+//       value: 5,
+//   },
+//   {
+//       name: "s04",
+//       value: 4,
+//   },
+//   {
+//       name: "s03",
+//       value: 3,
+//   },
+//   {
+//       name: "s02",
+//       value: 2,
+//   }
+// ];
 let dealerHand;
 let playerHand;
 let playerSum;
